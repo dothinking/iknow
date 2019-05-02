8f76d9ebc94837a8d672f211fac0af93aba5be43
dothinking <train8808@gmail.com>
2019-05-02 17:44:33 +0800

    update keywords

--- reader/deploy.py
+++ reader/deploy.py
@@ -9,7 +9,7 @@ dest_images = r'D:/WorkSpace/Gitee/iknow/public/uploads'
 
 # keywords
 keywords = {	
-	'极限': ['趋于'], 
+	'极限': ['趋于','趋近', '趋向'],
 	'等价无穷小': [],
 	'重要极限公式': [],
 	'夹逼准则': [],
@@ -24,7 +24,7 @@ keywords = {
 	'参数方程': [],
 	'弧长公式': [],
 	'曲率': [],
-	'偏导数': [],
+	'偏导数': ['偏导', '偏z'],
 	'全微分': [],
 	'方向导数': [],
 	'函数极值': ['极大值', '极小值', '极值'],
@@ -40,20 +40,20 @@ keywords = {
 	'广义坐标变换': [],
 	'积分中值定理': [],
 	'技巧型积分': [],
-	'奇偶对称性': [],
+	'奇偶对称性': ['奇函数在对称区间'],
 	'轮换对称性': [],
-	'去绝对值': [],
+	'去绝对值': ['去掉绝对值'],
 	'几何意义': [],
-	'交换积分次序': [],
-	'积分区间': ['积分上下限'],
+	'交换积分次序': ['积分次序', '换序'],
+	'积分区间': ['积分上下限', '积分上限'],
 	'极坐标系积分': ['极坐标系'],
 	'柱坐标系积分': [],
 	'球坐标系积分': [],
 	'广义坐标系积分': [],
 	'截面法': [],
 	'投影法': [],
-	'不定积分': [],
-	'三重积分': [],
+	'不定积分': ['求积分', '高数积分', '定积分', '∫', '求原函数'],
+	'三重积分': ['三次积分'],
 	'第一类曲线积分': [],
 	'第二类曲线积分': [],
 	'第一类曲面积分': ['面积积分', '曲面面积'],
@@ -72,25 +72,27 @@ keywords = {
 	'做功': [],
 	'矢量代数': [],
 	'切法向量': [],
-	'平面方程': [],
-	'直线方程': [],
+	'平面方程': ['切平面'],
+	'直线方程': ['切线方程', '法线方程'],
 	'曲面方程': [],
 	'点线面距离': ['距离'],
 	'点线面投影': ['的投影'],
 	'数项级数': [],
-	'级数展开': [],
+	'级数展开': ['展开为'],
 	'和函数': [],
 	'敛散性': [],
 	'收敛半径': [],
 	'分离变量法': [],
 	'一阶线性微分方程': [],
 	'二阶线性微分方程': [],
-	'matlab': ['MATLAB', 'Matlab']
+	'matlab': ['MATLAB', 'Matlab'],
+	'excel': ['Excel', 'EXCEL']
 }
 
 
 def tag_posts():
 	'''tag posts with pre-defined keywords'''
+	tag, untag = 0, 0
 	for fname in os.listdir(source_posts):
 		file_path = os.path.join(source_posts, fname)
 
@@ -111,14 +113,17 @@ def tag_posts():
 			if find:
 				tags.append(k)
 
-		# write text		
-		print(fname, ' with tags: ', tags if tags else 'UNTAGGED')
-
+		# write text
 		if not tags:
-			continue
-		text = text.replace('tags  : UNTAGGED', 'tags  : {0}'.format(' '.join(tags)))
-		with open(file_path, 'w', encoding='utf-8') as f:
-			f.write(text)
+			untag += 1
+			print(fname, ' with tags: UNTAGGED')
+		else:
+			tag += 1
+			print(fname, ' with tags: ', tags)
+			text = text.replace('tags  : UNTAGGED', 'tags  : {0}'.format(' '.join(tags)))
+			with open(file_path, 'w', encoding='utf-8') as f:
+				f.write(text)
+	return tag, untag
 
 
 
@@ -146,18 +151,17 @@ def move_images(posts_ids):
 
 if __name__ == '__main__':
 
-	print('Add tags according to keywords list...')
-	print('-'*30)
-	tag_posts()
-	
-	print('\nCopying posts and images to destination directories...')
-	# move all posts
-	posts_ids = move_posts()
-	# move associated images
-	m,n = move_images(posts_ids)
+	print('Add tags according to keywords list...')	
+	n_tag, n_untag = tag_posts()
+	print('-'*30) # summary
+	print('{0} posts are tagged.'.format(n_tag))
+	print('{0} posts are untagged.\n'.format(n_untag))
 
-	# summary
-	print('-'*30)
+	print('Copying posts and images to destination directories...')
+	
+	posts_ids = move_posts() # move all posts	
+	m,n = move_images(posts_ids) # move associated images	
+	print('-'*30) # summary
 	print('{0} posts are moved.'.format(len(posts_ids)))
 	print('{0} associated images are moved.'.format(m))
 	print('{0} unused images are deleted.'.format(n))
